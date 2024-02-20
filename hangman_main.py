768abe89ba1d4e220a7826838a91635ba1a8262b
Persephone <kissedbytherains@gmail.com>
2024-02-20 04:57:12 -0600

    Update and rename main.py to hangman_main.py

--- hangman_main.py
+++ hangman_main.py
@@ -4,18 +4,13 @@ import random
 import hangman_art
 import hangman_words
 
-#TODO-1: - Update the word list to use the 'word_list' from hangman_words.py
-#Delete this line: word_list = ["ardvark", "baboon", "camel"]
 chosen_word = random.choice(hangman_words.word_list)
 word_length = len(chosen_word)
 
 end_of_game = False
 lives = 6
 
-#TODO-3: - Import the logo from hangman_art.py and print it at the start of the game.
 print(hangman_art.logo)
-#Testing code
-print(f'Pssst, the solution is {chosen_word}.')
 
 #Create blanks
 display = []
@@ -24,8 +19,6 @@ for _ in range(word_length):
 
 while not end_of_game:
     guess = input("Guess a letter: ").lower()
-
-    #TODO-4: - If the user has entered a letter they've already guessed, print the letter and let them know.
 
     #Check guessed letter
     for position in range(word_length):
@@ -36,7 +29,6 @@ while not end_of_game:
 
     #Check if user is wrong.
     if guess not in chosen_word:
-        #TODO-5: - If the letter is not in the chosen_word, print out the letter and let them know it's not in the word.
         print(f"Your letter {guess} is not in the word, please try again.")
         lives -= 1
         if lives == 0:
@@ -51,5 +43,4 @@ while not end_of_game:
         end_of_game = True
         print("You win.")
 
-    #TODO-2: - Import the stages from hangman_art.py and make this error go away.
     print(hangman_art.stages[lives])
